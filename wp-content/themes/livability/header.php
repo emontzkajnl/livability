f217e39ae818125837d80633805bf67018b708fd
Eric Montzka <emontzka@jnlcom.com>
2023-09-14 13:41:09 -0500

    comment out google tag manager script

--- wp-content/themes/livability/header.php
+++ wp-content/themes/livability/header.php
@@ -26,14 +26,14 @@
 	<!-- End Google Tag Manager -->
 	<?php wp_head(); ?>
 	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-HVXCXLTZK1"></script>
+	<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-HVXCXLTZK1"></script>
 	<script>
 	window.dataLayer = window.dataLayer || [];
 	function gtag(){dataLayer.push(arguments);}
 	gtag('js', new Date());
 
 	gtag('config', 'G-HVXCXLTZK1');
-	</script>
+	</script> -->
 	<script>(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");ga("create", "UA-12112020-1", {"cookieDomain":"auto"});ga("set", "anonymizeIp", true);ga("send", "pageview");</script>
 	<?php if (!is_page('city-data-iframe')): ?>
 	<script src="https://ads.cordlessmedia.com/ad-wrapper/25419/cm.min.js"></script>
