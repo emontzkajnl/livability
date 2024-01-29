e7f458ac7083db71086dc31c2e7fc7ef3a7eefe8
Eric Montzka <emontzka@jnlcom.com>
2024-01-29 09:12:28 -0600

    add state coordinates

--- wp-content/plugins/jci-acf-blocks/blocks/find-bp2l/find-bp2l.php
+++ wp-content/plugins/jci-acf-blocks/blocks/find-bp2l/find-bp2l.php
@@ -77,55 +77,55 @@ $region = get_field('region_text');
     </div>
     <div class="bp2l__one-text-container">
         <ul class="bp2l__state-links">
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=3" target="proxi-state-map">Zoom out</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=8" target="proxi-state-map">Zoom in</a></li>
-            <!-- <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Arkansas</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">California</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Colorado</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Connecticut</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Delaware</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Florida</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Georgia</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Hawaii</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Idaho</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Illinois</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Indiana</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Iowa</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Kansas</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Kentucky</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Louisiana</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Maine</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Maryland</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Massachusetts</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Michigan</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Minnesota</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Mississippi</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Missouri</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Montana</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Nebraska</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Nevada</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">New Hampshire</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">New Jersey</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">New Mexico</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">New York</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">North Carolina</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">North Dakota</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Ohio</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Oklahoma</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Oregon</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Pennsylvania</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Rhode Island</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">South Carolina</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">South Dakota</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Tennessee</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Texas</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Utah</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Vermont</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Virginia</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Washington</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">West Virginia</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Wisconsin</a></li>
-            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I" target="proxi-state-map">Wyoming</a></li> -->
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=32.3182&lng=-86.9022" target="proxi-state-map">Alabama</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=5&lat=66.1605&lng=-153.3691" target="proxi-state-map">Alaska</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=34.7999&lng=-92.1999" target="proxi-state-map">Arkansas</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=6&lat=36.7782&lng=-119.4179" target="proxi-state-map">California</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=39.1130&lng=-105.3588" target="proxi-state-map">Colorado</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=9&lat=41.5999&lng=-72.6999" target="proxi-state-map">Connecticut</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=9&lat=39&lng=-75.5" target="proxi-state-map">Delaware</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=27.9944&lng=-81.7602" target="proxi-state-map">Florida</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=33.247875&lng=-83.441162" target="proxi-state-map">Georgia</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=19.741755&lng=-155.844437" target="proxi-state-map">Hawaii</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=44.068203&lng=-114.742043" target="proxi-state-map">Idaho</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=40&lng=-89" target="proxi-state-map">Illinois</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=40.273502&lng=-86.126976" target="proxi-state-map">Indiana</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=42.032974&lng=-93.581543" target="proxi-state-map">Iowa</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=38.5&lng=-98" target="proxi-state-map">Kansas</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=37.839333&lng=-84.270020" target="proxi-state-map">Kentucky</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=30.391830&lng=-92.329102" target="proxi-state-map">Louisiana</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=45.367584&lng=-68.972168" target="proxi-state-map">Maine</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=39.045753" target="proxi-state-map">Maryland</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=42.407211&lng=-71.382439" target="proxi-state-map">Massachusetts</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=44.182205&lng=-84.506836" target="proxi-state-map">Michigan</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=46.392410&lng=-94.636230" target="proxi-state-map">Minnesota</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=33&lng=-90" target="proxi-state-map">Mississippi</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=38.573936&lng=-92.603760" target="proxi-state-map">Missouri</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=46.965260&lng=-109.533691" target="proxi-state-map">Montana</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=41.5&lng=-100" target="proxi-state-map">Nebraska</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=39.876019&lng=-117.224121" target="proxi-state-map">Nevada</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=44&lng=-71.5" target="proxi-state-map">New Hampshire</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=39.833851&lng=-74.871826" target="proxi-state-map">New Jersey</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=34.307144&lng=-106.018066" target="proxi-state-map">New Mexico</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=43&lng=-75" target="proxi-state-map">New York</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=35.782169&lng=-80.793457" target="proxi-state-map">North Carolina</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=47.650589&lng=-100.437012" target="proxi-state-map">North Dakota</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=40.367474&lng=-82.996216" target="proxi-state-map">Ohio</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=36.084621&lng=-96.921387" target="proxi-state-map">Oklahoma</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=44&lng=-120.5" target="proxi-state-map">Oregon</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=41.203323&lng=-77.194527" target="proxi-state-map">Pennsylvania</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=41.742325&lng=-71.742332" target="proxi-state-map">Rhode Island</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=33.836082&lng=-81.163727" target="proxi-state-map">South Carolina</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=44.5&lng=-100" target="proxi-state-map">South Dakota</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=35.860119&lng=-86.660156" target="proxi-state-map">Tennessee</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=6&lat=31&lng=-100" target="proxi-state-map">Texas</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=39.419220&lng=-111.950684" target="proxi-state-map">Utah</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=44&lng=-72.699997" target="proxi-state-map">Vermont</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=37.926868&lng=-78.024902" target="proxi-state-map">Virginia</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=47.751076&lng=-120.740135" target="proxi-state-map">Washington</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=39&lng=-80.5" target="proxi-state-map">West Virginia</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=44.5&lng=-89.5" target="proxi-state-map">Wisconsin</a></li>
+            <li><a href="https://map.proxi.co/r/c-OsWkiWgcodPdEN1I2I?zoom=7&lat=43.075970&lng=-107.290283" target="proxi-state-map">Wyoming</a></li>
         </ul>
             <!-- <select name="States" id=""  size="12">
                 <optgroup label="Select a State">
