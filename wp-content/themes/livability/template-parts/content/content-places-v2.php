79f21d932f76bfd636a0c565a2da80e961ff9ef3
Eric Montzka <emontzka@jnlcom.com>
2024-03-07 11:01:25 -0600

    fix bp2l byline bug

--- wp-content/themes/livability/template-parts/content/content-places-v2.php
+++ wp-content/themes/livability/template-parts/content/content-places-v2.php
@@ -19,6 +19,53 @@
                 <?php get_template_part( 'template-parts/blocks/ad-one' ); ?>
             </div>
         </div>
+
+<div class="place-column__parent">
+    <div class="place-column__nav">
+    <ul class="place-side-nav">
+                       <li><a href="#top-100" >2024 Top 100</a></li>
+                       <li><a href="#things-to-do">Things to Do</a></li>
+                       <li><a href="#economy">Economy</a></li>
+                       <li><a href="brands-to-know">Brands to Know</a></li>
+                       <li><a href="quick-facts">Quick Facts</a></li>
+                       <li><a href="#weather">Weather</a></li>
+                       <li><a href="#map">Map</a></li>
+                       <li><a href="#experiences-and-adventures">Experiences & Adventures</a></li>
+                       <li><a href="#food-scenes">Food Scenes</a></li>
+                       <li><a href="#healthy-places">Healthy Places</a></li>
+                       <li><a href="#make-your-move">Make Your Move</a></li>
+                       <li><a href="#where-to-live-now">Where to Live Now</a></li>
+                       <li><a href="#education-careers-and-opportunities">Education, Careers & Opportunities</a></li>
+                       <li><a href="#love-where-you-live">Love Where You Live</a></li>
+                       <li><a href="#more-about-living-in">More About Living in </a></li>
+
+                   </ul>
+    </div>
+    <div class="place-column__content">
+    <div class="wp-block-columns">
+        <div class="wp-block-column">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sit amet nunc blandit, laoreet diam at, efficitur justo. Praesent cursus purus vel consectetur mattis. Nulla convallis, nisl in cursus pretium, nisl lacus vulputate mauris, quis tempor augue ante at augue. Vivamus imperdiet dictum sem, quis luctus leo tincidunt posuere. Mauris sed metus vehicula, pulvinar magna vitae, gravida mauris. Ut pellentesque, leo et efficitur feugiat, est velit accumsan mi, eget cursus mi nibh laoreet urna. Fusce et ultricies sem, eu malesuada purus. Nullam velit diam, imperdiet sed dui non, tincidunt laoreet lorem. Maecenas ac turpis laoreet, auctor sem a, congue nulla.
+
+Sed porta tortor et tellus elementum, ac imperdiet libero vestibulum. Nulla in ante lacus. Nam consequat orci vel elit lacinia lobortis sit amet vel nulla. Pellentesque eleifend in ante at tincidunt. Curabitur tincidunt hendrerit consectetur. In eu lectus vitae nunc sagittis ultrices non ut urna. Nulla ac iaculis nulla, nec rutrum risus. Duis nec auctor sapien. Donec sit amet risus elit. Quisque in erat sodales, scelerisque tortor vitae, mollis ex. Nulla ac iaculis nisl. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque fermentum augue nec tortor ultrices pulvinar. Nunc orci leo, luctus et dapibus vel, vestibulum a libero. Morbi vestibulum dapibus ligula id gravida.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sit amet nunc blandit, laoreet diam at, efficitur justo. Praesent cursus purus vel consectetur mattis. Nulla convallis, nisl in cursus pretium, nisl lacus vulputate mauris, quis tempor augue ante at augue. Vivamus imperdiet dictum sem, quis luctus leo tincidunt posuere. Mauris sed metus vehicula, pulvinar magna vitae, gravida mauris. Ut pellentesque, leo et efficitur feugiat, est velit accumsan mi, eget cursus mi nibh laoreet urna. Fusce et ultricies sem, eu malesuada purus. Nullam velit diam, imperdiet sed dui non, tincidunt laoreet lorem. Maecenas ac turpis laoreet, auctor sem a, congue nulla.
+
+Sed porta tortor et tellus elementum, ac imperdiet libero vestibulum. Nulla in ante lacus. Nam consequat orci vel elit lacinia lobortis sit amet vel nulla. Pellentesque eleifend in ante at tincidunt. Curabitur tincidunt hendrerit consectetur. In eu lectus vitae nunc sagittis ultrices non ut urna. Nulla ac iaculis nulla, nec rutrum risus. Duis nec auctor sapien. Donec sit amet risus elit. Quisque in erat sodales, scelerisque tortor vitae, mollis ex. Nulla ac iaculis nisl. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque fermentum augue nec tortor ultrices pulvinar. Nunc orci leo, luctus et dapibus vel, vestibulum a libero. Morbi vestibulum dapibus ligula id gravida.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sit amet nunc blandit, laoreet diam at, efficitur justo. Praesent cursus purus vel consectetur mattis. Nulla convallis, nisl in cursus pretium, nisl lacus vulputate mauris, quis tempor augue ante at augue. Vivamus imperdiet dictum sem, quis luctus leo tincidunt posuere. Mauris sed metus vehicula, pulvinar magna vitae, gravida mauris. Ut pellentesque, leo et efficitur feugiat, est velit accumsan mi, eget cursus mi nibh laoreet urna. Fusce et ultricies sem, eu malesuada purus. Nullam velit diam, imperdiet sed dui non, tincidunt laoreet lorem. Maecenas ac turpis laoreet, auctor sem a, congue nulla.
+
+Sed porta tortor et tellus elementum, ac imperdiet libero vestibulum. Nulla in ante lacus. Nam consequat orci vel elit lacinia lobortis sit amet vel nulla. Pellentesque eleifend in ante at tincidunt. Curabitur tincidunt hendrerit consectetur. In eu lectus vitae nunc sagittis ultrices non ut urna. Nulla ac iaculis nulla, nec rutrum risus. Duis nec auctor sapien. Donec sit amet risus elit. Quisque in erat sodales, scelerisque tortor vitae, mollis ex. Nulla ac iaculis nisl. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque fermentum augue nec tortor ultrices pulvinar. Nunc orci leo, luctus et dapibus vel, vestibulum a libero. Morbi vestibulum dapibus ligula id gravida.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sit amet nunc blandit, laoreet diam at, efficitur justo. Praesent cursus purus vel consectetur mattis. Nulla convallis, nisl in cursus pretium, nisl lacus vulputate mauris, quis tempor augue ante at augue. Vivamus imperdiet dictum sem, quis luctus leo tincidunt posuere. Mauris sed metus vehicula, pulvinar magna vitae, gravida mauris. Ut pellentesque, leo et efficitur feugiat, est velit accumsan mi, eget cursus mi nibh laoreet urna. Fusce et ultricies sem, eu malesuada purus. Nullam velit diam, imperdiet sed dui non, tincidunt laoreet lorem. Maecenas ac turpis laoreet, auctor sem a, congue nulla.
+
+Sed porta tortor et tellus elementum, ac imperdiet libero vestibulum. Nulla in ante lacus. Nam consequat orci vel elit lacinia lobortis sit amet vel nulla. Pellentesque eleifend in ante at tincidunt. Curabitur tincidunt hendrerit consectetur. In eu lectus vitae nunc sagittis ultrices non ut urna. Nulla ac iaculis nulla, nec rutrum risus. Duis nec auctor sapien. Donec sit amet risus elit. Quisque in erat sodales, scelerisque tortor vitae, mollis ex. Nulla ac iaculis nisl. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque fermentum augue nec tortor ultrices pulvinar. Nunc orci leo, luctus et dapibus vel, vestibulum a libero. Morbi vestibulum dapibus ligula id gravida.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sit amet nunc blandit, laoreet diam at, efficitur justo. Praesent cursus purus vel consectetur mattis. Nulla convallis, nisl in cursus pretium, nisl lacus vulputate mauris, quis tempor augue ante at augue. Vivamus imperdiet dictum sem, quis luctus leo tincidunt posuere. Mauris sed metus vehicula, pulvinar magna vitae, gravida mauris. Ut pellentesque, leo et efficitur feugiat, est velit accumsan mi, eget cursus mi nibh laoreet urna. Fusce et ultricies sem, eu malesuada purus. Nullam velit diam, imperdiet sed dui non, tincidunt laoreet lorem. Maecenas ac turpis laoreet, auctor sem a, congue nulla.
+
+Sed porta tortor et tellus elementum, ac imperdiet libero vestibulum. Nulla in ante lacus. Nam consequat orci vel elit lacinia lobortis sit amet vel nulla. Pellentesque eleifend in ante at tincidunt. Curabitur tincidunt hendrerit consectetur. In eu lectus vitae nunc sagittis ultrices non ut urna. Nulla ac iaculis nulla, nec rutrum risus. Duis nec auctor sapien. Donec sit amet risus elit. Quisque in erat sodales, scelerisque tortor vitae, mollis ex. Nulla ac iaculis nisl. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque fermentum augue nec tortor ultrices pulvinar. Nunc orci leo, luctus et dapibus vel, vestibulum a libero. Morbi vestibulum dapibus ligula id gravida.</p>
+
+        </div>
+    </div>
+</div>
+
+</div>
+
         <div class="wp-block-columns">
      
         </div>
@@ -36,24 +83,7 @@
                 </div>
                 <div class="wp-block-columns liv-columns-2">
                     <div class="wp-block-column">
-                   <ul class="place-side-nav">
-                       <li><a href="#top-100" ></a></li>
-                       <li><a href="#things-to-do"></a></li>
-                       <li><a href="#economy"></a></li>
-                       <li><a href="brands-to-know"></a></li>
-                       <li><a href="quick-facts"></a></li>
-                       <li><a href="#weather"></a></li>
-                       <li><a href="#map"></a></li>
-                       <li><a href="#experiences-and-adventures"></a></li>
-                       <li><a href="#food-scenes"></a></li>
-                       <li><a href="#healthy-places"></a></li>
-                       <li><a href="#make-your-move"></a></li>
-                       <li><a href="#where-to-live-now"></a></li>
-                       <li><a href="#education-careers-and-opportunities"></a></li>
-                       <li><a href="#love-where-you-live"></a></li>
-                       <li><a href="#more-about-living-in"></a></li>
 
-                   </ul>
                     <?php 
                     //get_template_part( 'template-parts/blocks/top-one-hundred-link'); ?>
                     
