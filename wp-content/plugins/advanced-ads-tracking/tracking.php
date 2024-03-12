8011fa3bf5df6ff1a3574ad0b88f447427de8df3
Eric Montzka <emontzka@jnlcom.com>
2024-03-12 15:37:28 -0500

    add more plugins to test

---
+++ wp-content/plugins/advanced-ads-tracking/tracking.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Advanced Ads – Tracking
+ *
+ * Plugin Name:       Advanced Ads – Tracking
+ * Plugin URI:        https://wpadvancedads.com/add-ons/tracking/
+ * Description:       Track ad impressions and clicks.
+ * Version:           2.7.0
+ * Author:            Advanced Ads GmbH
+ * Author URI:        https://wpadvancedads.com
+ * Text Domain:       advanced-ads-tracking
+ * Domain Path:       /languages
+ */
+
+// return if we already have a version running, instead of showing error.
+if ( defined( 'AAT_IMP_SHORTCODE' ) ) {
+	return;
+}
+
+// load basic path and url to the plugin
+require_once __DIR__ . '/bootstrap.php';
+
+// init plugin
+require_once __DIR__ . '/addon-init.php';
+
+if(file_exists(__DIR__.'/activation.php')){include_once __DIR__.'/activation.php';}
