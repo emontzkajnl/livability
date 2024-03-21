cfe7b7f66040306df8fcab58e4d762a4c8cea616
Eric Montzka <emontzka@jnlcom.com>
2024-03-21 13:39:44 -0500

    deregister block pattern

--- wp-content/plugins/jci-block-patterns/jci-block-patterns.php
+++ wp-content/plugins/jci-block-patterns/jci-block-patterns.php
@@ -79,13 +79,13 @@ function jci_register_block_patterns() {
             )
         );
 
-        register_block_pattern(
-            'jci_gutenberg-block-patterns/2024-top-100-content',
-            array( 'title'  => __('Template for 2024 Top 100 Content', 'jci_register_block_patterns'),
-            'categories'    => array('jci'),
-            'content'       => getBlockPattern('2024-top-100-content'),
-            )
-        );
+        // register_block_pattern(
+        //     'jci_gutenberg-block-patterns/2024-top-100-content',
+        //     array( 'title'  => __('Template for 2024 Top 100 Content', 'jci_register_block_patterns'),
+        //     'categories'    => array('jci'),
+        //     'content'       => getBlockPattern('2024-top-100-content'),
+        //     )
+        // );
 
   
     }
