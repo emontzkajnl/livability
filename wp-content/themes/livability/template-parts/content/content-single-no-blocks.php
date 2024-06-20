6b11d39e7894c8ff42c06fc2fb94bbf8165ff6e3
emontzka <emontzka@jnlcom.com>
2024-06-20 11:05:44 -0500

    center ad on single

--- wp-content/themes/livability/template-parts/content/content-single-no-blocks.php
+++ wp-content/themes/livability/template-parts/content/content-single-no-blocks.php
@@ -96,7 +96,7 @@
         <div class="wp-block-columns full-width-off-white">
             <div class="wp-block-column">
                 <?php //get_template_part('template-parts/blocks/ad-one' ); ?>
-                <div class="wp-block-jci-ad-area-one" >
+                <div class="wp-block-jci-ad-area-one" style="display: flex; justify-content: center;" >
                 <?php echo the_ad_group(698); ?>
                 </div>
             </div>
