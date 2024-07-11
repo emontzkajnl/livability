0a7b64ec06779c488210000aacae58e38d050ee7
emontzka <emontzka@jnlcom.com>
2024-07-11 11:29:42 -0500

    add fix to template

--- wp-content/themes/livability/template-parts/blocks/city-map.php
+++ wp-content/themes/livability/template-parts/blocks/city-map.php
@@ -5,6 +5,6 @@ $place_query = str_replace(' ','+', $place_query);
 if ($post->post_parent > 0):
 ?>
 <div class="city-map-block">
-    <iframe width="100%" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA-VaZnsUyefqTA8Nu4LZOB6rKJcQoTRgQ&q=<?php echo $place_query; ?>" allowfullscreen=""></iframe>
+    <iframe width="100%" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA-VaZnsUyefqTA8Nu4LZOB6rKJcQoTRgQ&q=<?php echo $place_query; ?>+USA" allowfullscreen=""></iframe>
 </div>
 <?php endif; ?>
