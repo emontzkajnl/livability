37e0a195677e6228d7ab051a487045f2452ed062
emontzka <emontzka@jnlcom.com>
2024-08-07 15:27:09 -0500

    fix prvs commit

--- wp-content/themes/livability/single.php
+++ wp-content/themes/livability/single.php
@@ -67,7 +67,7 @@ while ( have_posts() ) :
 			$place_rel_query = '';
 		}
 		// echo 'place rel query '.$place_rel_query; ?>
-	<?php echo do_shortcode( '[ajax_load_more single_post="true" single_post_id="'.get_the_ID().'" '.$place_rel_query.' single_post_order="previous" single_post_taxonomy="category"  theme_repeater="alm_single_template"  post_type="post" pause_override="true" loading_style="infinite classic" scroll_distance="100"]' );
+	<?php echo do_shortcode( '[ajax_load_more single_post="true" single_post_id="'.get_the_ID().'" '.$place_rel_query.' single_post_order="previous" single_post_taxonomy="category"  theme_repeater="alm_single_template.php"  post_type="post" pause_override="true" loading_style="infinite classic" scroll_distance="100"]' );
 
 endwhile; // End of the loop.
 
