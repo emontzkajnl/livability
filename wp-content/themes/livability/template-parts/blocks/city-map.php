50a7af53db2f32f4f10b3e4e8063b9fc61e2ec07
emontzka <emontzka@jnlcom.com>
2025-01-22 10:04:28 -0600

    add map options fields

--- wp-content/themes/livability/template-parts/blocks/city-map.php
+++ wp-content/themes/livability/template-parts/blocks/city-map.php
@@ -1,10 +1,22 @@
 <?php 
+if ($post->post_parent > 0){
+    if (get_field('city_map_options')) {
+    $place_query = str_replace(',','',get_the_title());
+    $place_query = str_replace(' ','+', $place_query); 
+    $cmo = get_field('city_map_options');
+    $option = $cmo['options']; // title, coordinates, proxi, hide
+    $zoom = $cmo['set_zoom_level'] ? '&zoom='.$cmo['zoom_level']: '';
 
-$place_query = str_replace(',','',get_the_title());
-$place_query = str_replace(' ','+', $place_query); 
-if ($post->post_parent > 0):
+    ?>
+    <div class="city-map-block">
+        <iframe width="100%" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA-VaZnsUyefqTA8Nu4LZOB6rKJcQoTRgQ&q=<?php echo $place_query; ?>+USA<?php echo $zoom; ?>" allowfullscreen=""></iframe>
+    </div>
+    <?php } else { ?>
+         
+    <div class="city-map-block">
+        <iframe width="100%" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA-VaZnsUyefqTA8Nu4LZOB6rKJcQoTRgQ&q=<?php echo $place_query; ?>+USA" allowfullscreen=""></iframe>
+    </div>
+
+    <?php }
+}
 ?>
-<div class="city-map-block">
-    <iframe width="100%" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA-VaZnsUyefqTA8Nu4LZOB6rKJcQoTRgQ&q=<?php echo $place_query; ?>+USA" allowfullscreen=""></iframe>
-</div>
-<?php endif; ?>
