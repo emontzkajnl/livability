ae5152289bad27e682707371e5007d40f1f27c5c
emontzka <emontzka@jnlcom.com>
2025-01-23 14:36:46 -0600

    add to map options block

--- wp-content/themes/livability/template-parts/blocks/city-map.php
+++ wp-content/themes/livability/template-parts/blocks/city-map.php
@@ -2,22 +2,35 @@
 if ($post->post_parent > 0){
     $place_query = str_replace(',','',get_the_title());
     $place_query = str_replace(' ','+', $place_query); 
+    echo '<div class="city-map-block">';
     if (get_field('city_map_options')) {
 
     $cmo = get_field('city_map_options');
     $option = $cmo['options']; // title, coordinates, proxi, hide
     $zoom = $cmo['set_zoom_level'] ? '&zoom='.$cmo['zoom_level']: '';
-
+    
+    switch ($option) {
+        case 'title': 
+                echo '<iframe width="100%" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA-VaZnsUyefqTA8Nu4LZOB6rKJcQoTRgQ&q='.$place_query.'+USA'.$zoom.'" allowfullscreen=""></iframe>';
+            break;
+        case 'coordinates':
+            $lat = $cmo['latitude'];
+            $long = $cmo['longitude'];
+            echo '<iframe width="100%" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA-VaZnsUyefqTA8Nu4LZOB6rKJcQoTRgQ&q='.$place_query.'&center='.$lat.','.$long.$zoom.'" allowfullscreen=""></iframe>';
+            break;
+        case 'proxi':
+            $proxi = $option['proxi'];
+            echo '<iframe src="'.$proxi.$zoom.'" allow="geolocation; clipboard-write" width="100%" height="625px" style="border-width: 0px;" allowfullscreen></iframe>';
+            break;
+    
+        default:
+            // hide map
+            break;
+    }
     ?>
-    <div class="city-map-block">
-        <iframe width="100%" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA-VaZnsUyefqTA8Nu4LZOB6rKJcQoTRgQ&q=<?php echo $place_query; ?>+USA<?php echo $zoom; ?>" allowfullscreen=""></iframe>
-    </div>
-    <?php } else { ?>
-         
-    <div class="city-map-block">
-        <iframe width="100%" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA-VaZnsUyefqTA8Nu4LZOB6rKJcQoTRgQ&q=<?php echo $place_query; ?>+USA" allowfullscreen=""></iframe>
-    </div>
-
-    <?php }
+    <?php } else { // no city map options 
+        echo '<iframe width="100%" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA-VaZnsUyefqTA8Nu4LZOB6rKJcQoTRgQ&q='.$place_query.'+USA'.$zoom.'" allowfullscreen=""></iframe>';
+    }
+    echo '</div>';
 }
 ?>
