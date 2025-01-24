620b9312ef01bad3f4a7b94da797e1b6c2bab53b
emontzka <emontzka@jnlcom.com>
2025-01-24 09:50:10 -0600

    fix proxi map

--- wp-content/themes/livability/template-parts/blocks/city-map.php
+++ wp-content/themes/livability/template-parts/blocks/city-map.php
@@ -6,7 +6,7 @@ if ($post->post_parent > 0){
     if (get_field('city_map_options')) {
 
     $cmo = get_field('city_map_options');
-    $option = $cmo['options']; // title, coordinates, proxi, hide
+    $option = $cmo['options']; // title, coordinates, proxi_url, hide
     $zoom = $cmo['set_zoom_level'] ? '&zoom='.$cmo['zoom_level']: '';
     
     switch ($option) {
@@ -19,7 +19,7 @@ if ($post->post_parent > 0){
             echo '<iframe width="100%" height="500" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA-VaZnsUyefqTA8Nu4LZOB6rKJcQoTRgQ&q='.$place_query.'&center='.$lat.','.$long.$zoom.'" allowfullscreen=""></iframe>';
             break;
         case 'proxi':
-            $proxi = $cmo['proxi'];
+            $proxi = $cmo['proxi_url'];
             echo '<iframe src="'.$proxi.'" allow="geolocation; clipboard-write" width="100%" height="625px" style="border-width: 0px;" allowfullscreen></iframe>';
             break;
     
