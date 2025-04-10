a901dbdb445184893c091dfa8a3edae65760ded7
emontzka <emontzka@jnlcom.com>
2025-04-10 15:15:35 -0500

    more bugherd

--- wp-content/themes/livability/template-parts/content/content-places-v2.php
+++ wp-content/themes/livability/template-parts/content/content-places-v2.php
@@ -76,8 +76,8 @@ $topic_args = array(
         <li class="active"><a href="#overview">Overview</a></li>
         <?php if ($is_2024_bp): ?>
         <li><a href="#top-100" >2024 Top 100</a></li>
-        <li><a href="#things-to-do">Things to Do</a></li>
-        <li><a href="#economy">Economy</a></li>
+        <!-- <li><a href="#things-to-do">Things to Do</a></li>
+        <li><a href="#economy">Economy</a></li> -->
         <?php endif; 
         echo count($brands_array) > 0 ? '<li><a href="#brands-to-know">Brands to Know</a></li>' : '';
         echo $hide_weather ? '' : '<li><a href="#weather">Weather</a></li>'; 
