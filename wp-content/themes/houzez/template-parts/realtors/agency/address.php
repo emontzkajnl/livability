e88d5e959c64791ac665f44ede6bef4470a4cf58
emontzka <emontzka@jnlcom.com>
2025-10-21 09:42:29 -0500

    remove houzez from repo and add to gitignore

--- wp-content/themes/houzez/template-parts/realtors/agency/address.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$agency_address = get_post_meta( get_the_ID(), 'fave_agency_address', true );
-if(!empty($agency_address)) {
-	echo '<address class="mb-1" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress"><i class="houzez-icon icon-pin"></i> <span itemprop="streetAddress">'.$agency_address.'</span></address>';
-}
